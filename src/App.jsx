00fe1feaa25fcfdedd4f1a1baf7b37afa2d98309
Md Kibria <mdkibria684@gmail.com>
2023-06-27 13:39:00 +0600

    Completed the project

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+
+import Photo from './assets/notice.png'
+import { useEffect, useRef, useState } from 'react';
+import html2canvas from 'html2canvas';
+
+function App() {
+  const main = useRef()
+  const btn = useRef()
+  const date = useRef()
+  const d = new Date();
+
+  const [notice, setNotice] = useState('')
+  const [author, setAuthor] = useState('')
+
+  useEffect(() => {
+    date.current.innerHTML = `Created On: ${d}`;
+
+    if(localStorage.getItem('nm_author')) {
+      setAuthor(localStorage.getItem('nm_author'));
+    }
+  }, [])
+
+  useEffect(() => {
+    html2canvas(main.current).then(function(canvas) {
+      const imageData = canvas.toDataURL("image/jpg");
+      const newData = imageData.replace(/^data:image\/jpg/, "data:application/octet-stream");
+
+      
+      btn.current.setAttribute("download", "notice.jpg");
+      btn.current.setAttribute("href", newData);
+    })
+  }, [notice, author])
+
+  const handleAuthor = (e) => {
+    setAuthor(e.target.value)
+    localStorage.setItem('nm_author', e.target.value);
+  }
+
+  return (
+    <div className="body">
+      <h1>Notice Maker <span>(Mgt&Mkt Group)</span></h1>
+      <div className="controls">
+        <textarea onChange={(e) => setNotice(e.target.value)} value={notice} placeholder='তোমার নোটিশ লেখ' />
+        <input type="text" onChange={handleAuthor} value={author} placeholder='তোমার নাম লেখ' />
+      </div>
+      
+      <span className='preview'>Preview:</span>
+      <div ref={main} className="main">
+        <img src={Photo} alt="" />
+        <p className="date">{new Date().toLocaleDateString('bn')}</p>
+        <p className="notice">{notice}</p>
+        <p className="author">{author}</p>
+        <p ref={date} className="create"></p>
+      </div>
+      
+      <a className="btn" ref={btn}>Download</a>
+      <p className="footer">&copy;All rights reserved by <a target='_blank' href="https://www.facebook.com/kibria.prodev">Md Kibria</a></p>
+    </div>
+  )
+}
+
+export default App
